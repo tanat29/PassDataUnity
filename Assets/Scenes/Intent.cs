88bdc761c1fc9063b3e73f393da56c607831371e
tanat29 <tanat29.mongkon@gmail.com>
2021-11-25 13:37:10 +0700

    first commit

---
+++ Assets/Scenes/Intent.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Intent : MonoBehaviour
+{
+    
+    public void Goto() {
+        PlayerPrefs.SetInt ("data", 7);
+        SceneManager.LoadScene("Scene2");
+    }
+}
---
+++ Assets/Scenes/receive_data.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class receive_data : MonoBehaviour
+{
+    public Text score_text;
+    private int old_score;
+
+    void Start () {    
+        old_score = PlayerPrefs.GetInt ("data");
+        score_text.text = old_score.ToString ();      
+    }
+}
